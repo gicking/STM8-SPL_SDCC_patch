fb0477bf09b216fd511d77761c113be8c30d72fd
Georg Icking-Konert <icking@onlinehome.de>
2017-08-16 17:03:52 +0200

    added option for new STM8L10x patch
    added option for new STM8L10x patch

--- apply_patch.sh
+++ apply_patch.sh
@@ -4,7 +4,8 @@
 cd `dirname $0`
 
 # set folders to patch 
-PATCHFILE=./STM8_SPL_SDCC.patch
+#PATCHFILE=./STM8L10x_StdPeriph_Lib_sdcc.patch
+PATCHFILE=./STM8S_StdPeriph_Lib_sdcc.patch
 
 # use patch command to apply patchfile 
 # https://stackoverflow.com/questions/9980186/how-to-create-a-patch-for-a-whole-directory-to-update-it
