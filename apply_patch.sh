6756f8d05b32f30327416cc2cebd0ad8c0bac31c
gicking <icking@onlinehome.de>
2018-07-24 21:09:11 +0200

    add SPL base versions to names of patchfiles
    add SPL base versions to names of patchfiles

--- apply_patch.sh
+++ apply_patch.sh
@@ -4,8 +4,10 @@
 cd `dirname $0`
 
 # set folders to patch 
-#PATCHFILE=./STM8L10x_StdPeriph_Lib_sdcc.patch
-PATCHFILE=./STM8S_StdPeriph_Lib_sdcc.patch
+#PATCHFILE=./STM8L10x_StdPeriph_Lib_V1.2.1_sdcc.patch
+#PATCHFILE=./STM8L15x-16x-05x-AL31-L_StdPeriph_Lib_V1.6.2_sdcc.patch
+PATCHFILE=./STM8S_StdPeriph_Lib_V2.3.1_sdcc.patch
+#PATCHFILE=./STM8TL5x_StdPeriph_Lib_V1.0.1.patch
 
 # use patch command to apply patchfile 
 # https://stackoverflow.com/questions/9980186/how-to-create-a-patch-for-a-whole-directory-to-update-it
