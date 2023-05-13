c276f87243c7e35588e92c686e2728ee8da2db1a
gicking <icking@onlinehome.de>
2023-05-13 10:16:17 +0200

    added chmod to patch script

--- apply_patch.sh
+++ apply_patch.sh
@@ -10,14 +10,16 @@ cd `dirname $0`
 echo off
 clear
 
-# set folders to patch 
+# set active patchfile 
 #PATCHFILE=./STM8L10x_StdPeriph_Lib_V1.2.1_sdcc.patch
 #PATCHFILE=./STM8L15x-16x-05x-AL31-L_StdPeriph_Lib_V1.6.2_sdcc.patch
 PATCHFILE=./STM8S_StdPeriph_Lib_V2.3.1_sdcc.patch
 #PATCHFILE=./STM8TL5x_StdPeriph_Lib_V1.0.1.patch
 
-# use patch command to apply patchfile 
-# https://stackoverflow.com/questions/9980186/how-to-create-a-patch-for-a-whole-directory-to-update-it
+# recursively set write permissions. Some seem to be broken
+chmod -R u+w STM8*
+
+# use patch command to apply patchfile. See https://stackoverflow.com/questions/9980186/how-to-create-a-patch-for-a-whole-directory-to-update-it
 #   -p0 == needed to find the proper folder
 patch -p0 < $PATCHFILE
 
